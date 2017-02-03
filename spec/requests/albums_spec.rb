68c88dcd61a5d2d4a2cd51942491afa786dac084
antoinfive <antoinfive@gmail.com>
2017-02-03 08:39:43 -0500

    make changes to album spec

--- spec/requests/albums_spec.rb
+++ spec/requests/albums_spec.rb
@@ -1,39 +1,39 @@
-require 'rails_helper' 
+require 'rails_helper'
 
-describe "Albums API" do 
-  it 'returns a JSON collection all of albums' do 
-    FactoryGirl.create_list(:album, 25) 
+describe "Albums API" do
+  it 'returns a JSON collection all of albums' do
+    FactoryGirl.create_list(:album, 25)
 
     get '/v1/albums'
 
-    json = JSON.parse(response.body) 
+    json = JSON.parse(response.body)
 
-    expect(response).to be_success 
+    expect(response).to be_success
 
     expect(json.length).to eq(25)
   end
 
-  describe 'get /v1/albums/:id' do 
-    it '' do 
-      # album = FactoryGirl.create(:album) 
+  describe 'get /v1/albums/:id' do
+    it 'returns a JSON object representing an album' do
+      # album = FactoryGirl.create(:album)
       album  = Album.create(title: 'I See You', release_date: Date.today, genre: 'Indie')
 
       get "/v1/albums/#{album.id}"
 
       json = JSON.parse(response.body)
 
-      expect(response).to be_success 
-      expect(json['id']).to eq(1) 
+      expect(response).to be_success
+      expect(json['id']).to eq(1)
       expect(json['title']).to eq(album.title)
     end
   end
 
- describe 'post /v1/albums' do 
-    it 'creates a album from post request' do 
-      data = { album: { title: 'Sylvan Esso' } } 
+ describe 'post /v1/albums' do
+    it 'creates an album from post request' do
+      data = { album: { title: 'Sylvan Esso' } }
       count = Album.count
 
-      post "/v1/albums/", params: data 
+      post "/v1/albums/", params: data
       json = JSON.parse(response.body)
       expect(response).to have_http_status(201)
 
@@ -42,36 +42,36 @@ describe "Albums API" do
     end
  end
 
- describe 'put /v1/albums/:id' do 
-    it 'updates a album from put request' do 
-      album = FactoryGirl.create(:album) 
+ describe 'put /v1/albums/:id' do
+    it 'updates an album from put request' do
+      album = FactoryGirl.create(:album)
       title = album.title
       new_album_data = { album: {id: album.id, title: 'XO'} }
 
-      put "/v1/albums/#{album.id}", params: new_album_data 
+      put "/v1/albums/#{album.id}", params: new_album_data
       json = JSON.parse(response.body)
-    
+
       expect(title).to_not eq(json['title'])
       expect(response).to have_http_status(200)
     end
  end
 
- describe 'destroy /v1/albums/:id' do 
-  it 'destroys the albums record' do 
+ describe 'destroy /v1/albums/:id' do
+  it 'destroys the albums record' do
     album =  Album.create(title: 'Purpose')
     delete "/v1/albums/#{album.id}"
-    
-    expect(response).to be_success 
+
+    expect(response).to be_success
 
     expect(Album.count).to eq(0)
   end
  end
 
- describe 'Resource not found' do 
-   it 'returns 404 when album is not found' do 
-      get "/v1/albums/9000" 
+ describe 'Resource not found' do
+   it 'returns 404 when album is not found' do
+      get "/v1/albums/9000"
 
-      expect(response.status).to eq(404) 
+      expect(response.status).to eq(404)
    end
  end
 end
